ffafbaf5405d2c7eb654fc3bb223d012679a0f41
Meeran B <v9@live.co.uk>
2020-10-13 00:20:01 +0100

    Challenge 3 solution

--- workshop/counter.js
+++ workshop/counter.js
@@ -1,12 +1,24 @@
 import h from "./create-element.js"
 
+let count = 0
+
+function setCount(newCount) {
+  count = newCount
+  app.innerHTML = ``
+  app.append(counter());
+}
 
 function counter() {
-  const view = h("span", {}, "0")
-  const button = h("button", { onclick: () => {
-    view.textContent = parseInt(view.textContent) + 1;
-  }}, "+")
-  return h("div", {}, view, button)
+  const view = h("span", {}, count)
+  const inc = () => {
+    setCount(count + 1)
+  }
+  const dec = () => {
+    setCount(count - 1)
+  }
+  const minusButton = h("button", { onclick: dec, disabled: count < 1}, "-")
+  const button = h("button", { onclick: inc, disabled: count === 9 }, "+")
+  return h("div", {}, minusButton, view, button)
 }
 
 // put the elements onto the page
