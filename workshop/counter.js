5dfe2841f6c9c0928bd5c780353320188b28525c
Meeran B <v9@live.co.uk>
2020-10-12 23:41:29 +0100

    Challenge 1 solution added

--- workshop/counter.js
+++ workshop/counter.js
@@ -1,15 +1,22 @@
-const view = document.createElement("span");
-view.append("0");
 
-const button = document.createElement("button");
-button.append("+");
-button.onclick = () => {
-  view.textContent = parseInt(view.textContent) + 1;
-};
+function counter() {
+  const view = document.createElement("span");
+  view.append("0");
 
-const counter = document.createElement("div");
-counter.append(view, button);
+  const button = document.createElement("button");
+  button.append("+");
+
+
+  button.onclick = () => {
+    view.textContent = parseInt(view.textContent) + 1;
+  };
+
+  const counter = document.createElement("div");
+  counter.append(view, button);
+
+  return counter;
+}
 
 // put the elements onto the page
 const app = document.querySelector("#app");
-app.append(counter);
+app.append(counter());
